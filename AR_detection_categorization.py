25cf33156aaf5684d8d68698dc5900be40debe19
Diego Vega Gimenez <diegovegagimenez@gmail.com>
2023-01-04 13:36:36 +0100

    Add files via upload
    AR_detection_categorization.py script allows to characterize AR-events, classifying them in five categories (AR1 to AR5) according to their intensity and persistence. AR1 events are primarily beneficial and frequent; AR2, AR3 and AR4 include a mix of beneficial and hazardous impacts, while AR5 are mainly hazardous and very infrequent. The scale makes uses of the water vapor transport (IVT) obtained from the ERA5 reanalysis model. From a purely Eulerian perspective, the proposed algorithm is applied to each one of the domain’s grid points from 1979 to 2019.
    
    Download_IVT_6h.py and Download_Pr_1h.py scripts allows to download NetCDF files of different variables directly from Copernicus web database. (https://www.copernicus.eu/en/access-data)

---
+++ AR_detection_categorization.py
@@ -0,0 +1,134 @@
+# Environmental Physics Laboratory (EPhysLab), Universidade de Vigo 2020
+# Simplified script SCALE ARs. Hourly version. 5 days of data.
+
+
+import numpy as np
+import time, datetime, sys
+import multiprocessing
+from multiprocessing import *
+import sys
+from netCDF4 import Dataset
+
+File = Dataset(
+    "./ERA5_ARs2019_Case_Study.nc")  # Data from ERA5 over the region of interest. This data contains 5 days.
+# The analysis will be carried out the second, third and fourth day (72 hours).
+
+# This script does not use XARRAY and Cartopy. It makes use of numpy and generates data designed to plot in Basemap.
+# Thus, a latitudinal correction ::-1 is necessary.
+
+Lats = File.variables["latitude"][::-1]
+
+print(Lats)
+Lons = File.variables["longitude"][:]
+East = File.variables["p71.162"][:, ::-1, :]
+North = File.variables["p72.162"][:, ::-1, :]
+IVT = np.sqrt(East ** 2 + North ** 2)
+
+l1 = 250
+l2 = 500
+l3 = 750
+l4 = 1000
+l5 = 1250
+
+
+def Eventer(max_IVT, persistence):  # EVENT CLASIFICATION MODULE. Time Resolution: 1h
+
+    CAT = 0
+
+    if max_IVT >= l1 and max_IVT < l2 and persistence < 24:
+        CAT = 0
+    if max_IVT >= l1 and max_IVT < l2 and persistence >= 24 and persistence < 48:
+        CAT = 1
+    if max_IVT >= l1 and max_IVT < l2 and persistence >= 48:
+        CAT = 2
+
+    if max_IVT >= l2 and max_IVT < l3 and persistence < 24:
+        CAT = 1
+    if max_IVT >= l2 and max_IVT < l3 and persistence >= 24 and persistence < 48:
+        CAT = 2
+    if max_IVT >= l2 and max_IVT < l3 and persistence >= 48:
+        CAT = 3
+
+    if max_IVT >= l3 and max_IVT < l4 and persistence < 24:
+        CAT = 2
+    if max_IVT >= l3 and max_IVT < l4 and persistence >= 24 and persistence < 48:
+        CAT = 3
+    if max_IVT >= l3 and max_IVT < l4 and persistence >= 48:
+        CAT = 4
+
+    if max_IVT >= l4 and max_IVT < l5 and persistence < 24:
+        CAT = 3
+    if max_IVT >= l4 and max_IVT < l5 and persistence >= 24 and persistence < 48:
+        CAT = 4
+    if max_IVT >= l4 and max_IVT < l5 and persistence >= 48:
+        CAT = 5
+
+    if max_IVT >= l5 and persistence < 24:
+        CAT = 4
+    if max_IVT >= l5 and persistence >= 24 and persistence < 48:
+        CAT = 5
+    if max_IVT >= l5 and persistence >= 48:
+        CAT = 5
+
+    return CAT
+
+
+def Main_Analyzer(Array):
+    EVENTS = np.zeros(shape=(Array.shape[0]))
+
+    t = 0
+
+    print(Array.shape)
+
+    while t < Array.shape[0]:
+
+        if Array[t] < l1:
+            t = t + 1
+        elif Array[t] >= l1:
+
+            event_IVT = []
+            event_t = []
+            event_IVT = np.array(event_IVT)
+            event_t = np.array(event_t)
+
+            event_IVT = np.append(event_IVT, Array[t])
+            event_t = np.append(event_t, t)
+
+            go = 1
+            t = t + 1
+            while go == 1:
+                if t < Array.shape[0]:
+
+                    if Array[t] >= l1:
+                        event_IVT = np.append(event_IVT, Array[t])
+                        event_t = np.append(event_t, t)
+                        t = t + 1
+                    else:
+                        go = 0
+                        CAT = Eventer(np.max(event_IVT), len(event_t))
+                        for i in event_t:
+                            EVENTS[int(i)] = CAT
+
+                else:
+                    go = 0
+                    CAT = Eventer(np.max(event_IVT), len(event_t))
+                    for i in event_t:
+                        EVENTS[int(i)] = CAT
+
+    return EVENTS
+
+
+OUTPUT = np.zeros(shape=(Lats.shape[0], Lons.shape[0]))
+
+for n in range(Lats.shape[0]):
+    print("%s out of %s" % (n, Lats.shape[0] - 1))
+    for m in range(Lons.shape[0]):
+        ARRAY = IVT[:, n, m]
+        ARRAY = Main_Analyzer(ARRAY)
+        ARRAY_72h = ARRAY[24:-24]
+        max = np.max(ARRAY_72h)
+        OUTPUT[n, m] = max
+
+np.save("Output_Detections_Categories_v2", OUTPUT)
+# This output provides the max category detected at each point throughout the 72-hour period
+# Remember that latitudes have been transformed with respect to the original ERA5 Grid.
---
+++ Download_IVT_6h.py
@@ -0,0 +1,43 @@
+import cdsapi
+import sys
+
+Year = sys.argv[1]
+
+print("%s"%Year)
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': [
+            'vertical_integral_of_eastward_water_vapour_flux', 'vertical_integral_of_northward_water_vapour_flux',
+        ],
+        'year': Year,
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': [
+            '00:00', '06:00', '12:00',
+            '18:00',
+        ],
+    },
+    'ERA5_Original_6h_East_West_%s.nc'%Year)
